d88b8fa50f0b4dad9aeb3a209fd574f3ebc8f092
mirokosz <michalrokosz663166610@gmail.com>
2023-04-04 20:18:14 +0200

    reating the StandardGradeBook class

---
+++ GradeBook/GradeBooks/StandardGradeBook.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GradeBook.GradeBooks
+{
+    public class StandardGradeBook : BaseGradeBook
+    {
+        public StandardGradeBook(string name):base(name)
+        {
+            Name = name;
+            Type=Enums.GradeBookType.Standard;
+        }
+    }
+}
