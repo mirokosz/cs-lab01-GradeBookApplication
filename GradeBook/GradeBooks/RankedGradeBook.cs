6ca854ea600934a5080a6a46c58b7d08d1337c61
mirokosz <michalrokosz663166610@gmail.com>
2023-04-06 12:29:50 +0200

    Override RankedGradeBook's CalculateStudentStatistics method

--- GradeBook/GradeBooks/RankedGradeBook.cs
+++ GradeBook/GradeBooks/RankedGradeBook.cs
@@ -61,5 +61,16 @@ namespace GradeBook.GradeBooks
                     return 'F';
             }
         }
+        public virtual void CalculateStatistics()
+        {
+            if (Students.Count() < 5)
+            {
+                Console.WriteLine("Ranked grading requires at least 5 students.");
+            }
+            else
+            {
+                base.CalculateStatistics();
+            }
+        }
     }
 }
